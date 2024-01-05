749e138df006ec62de72334d284d5b733c747110
Jon C <me@jonc.dev>
2024-01-05 15:31:42 +0100

    Simplify tmux powerline theme

--- tmux-powerline/themes/my-theme.sh
+++ tmux-powerline/themes/my-theme.sh
@@ -84,10 +84,10 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
-		"vcs_branch 29 88" \
+		"vcs_branch 24 250" \
 		#"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
+		"vcs_modified 9 250" \
 		#"vcs_others 245 0" \
 	)
 fi
@@ -95,12 +95,12 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		"pwd 89 250" \
 		#"macos_notification_count 29 255" \
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
+		"load 237 175" \
 		#"tmux_mem_cpu_load 234 136" \
 		#"battery 137 127" \
 		#"weather 37 255" \
