d4597aabf8966cd37290ef5375aa70b4e44e1408
Jon C <me@jonc.dev>
2024-03-03 14:06:54 +0100

    setup: Update script to work for more distros

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 DIR=$(dirname "$0")
 cd "$DIR" || exit
 FULLDIR=$(pwd)
 
-INSTALL_GUI=false
+INSTALL_GUI=true
 INSTALL_EXTRA=false
-RASPBERRY_PI=false
-RELEASE=$(lsb_release -is)
+
+UPDATE_COMMAND="pacman --noconfirm -Syu"
+INSTALL_COMMAND="pacman --noconfirm -S"
 
 echo "Base dev software"
 echo "* Install base requirements"
-sudo apt install -y curl git tmux neovim lld
+sudo $UPDATE_COMMAND
 
-echo "* Install fish"
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-sudo apt update
-sudo apt install -y fish
+echo "* Install base dev stuff"
+sudo $INSTALL_COMMAND curl git tmux neovim fish base-devel xsel lld ctags
+
+echo "* Switch to fish"
+chsh -s /usr/bin/fish
 
 # Diff and setup each config
 echo "Setting up all config files as symlinks"
@@ -30,7 +31,7 @@ do
     diff ~/$FILE "$FULLDIR"/$FILE
     rm -i ~/$FILE
   fi
-  "* Setting up link to $FULLDIR/$FILE"
+  echo "* Setting up link to $FULLDIR/$FILE"
   ln -s "$FULLDIR"/$FILE ~
 done
 echo "* Setting up link to $FULLDIR/.ssh/config"
@@ -41,206 +42,137 @@ echo "* Setting up link to $FULLDIR/init.vim"
 mkdir -p ~/.config/nvim
 ln -s "$FULLDIR"/init.vim ~/.config/nvim
 
+echo "* Setting up link from vim to nvim"
+sudo ln -s /usr/bin/nvim /usr/bin/vim
+sudo ln -s /usr/bin/nvim /usr/bin/vi
+
 echo "* Setting up link to $FULLDIR/config.fish"
 mkdir -p ~/.config/fish
-ln -s "$FULLDIR"/config.fish ~/.config/fish
+ln -s "$FULLDIR"/config.fish ~/.config/fish/config.fish
+
+echo "* Setting up link to $FULLDIR/flake8"
+ln -s "$FULLDIR"/flake8 ~/.config
 
 echo "* Setting up link to $FULLDIR/1.ctags"
 mkdir -p ~/.config/ctags
 ln -s "$FULLDIR"/1.ctags ~/.config/ctags
 
-echo "* Setting up link to $FULLDIR/flake8"
-ln -s "$FULLDIR"/flake8 ~/.config
-
 echo "* Setting up link to add emojis to Hack"
 mkdir -p ~/.config/fontconfig/conf.d
 ln -s "$FULLDIR"/fontconfig/99-hack-color-emoji.conf ~/.config/fontconfig/conf.d
 
+echo "* Setting up link to $FULLDIR/git-cliff"
+ln -s "$FULLDIR"/git-cliff ~/.config/
+
 echo "* Setting up Plugged for vim plugins in init.vim"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Coding software"
 echo "* Install python dev requirements"
-sudo apt install -y python3-dev python3-pip python3-venv
+sudo $INSTALL_COMMAND python python-pip python-pipenv
 
 echo "* Install global pynvim, flake8, mypy"
-sudo pip3 install pynvim flake8 mypy
+sudo pip install pynvim flake8 mypy
 
-echo "* Install and setup powerline-status"
-pip install git+https://github.com/powerline/powerline
-pip install pomo-fish-powerline
-echo "* Setting up link to $FULLDIR/powerline"
-ln -s "$FULLDIR"/powerline ~/.config
+echo "* Install and setup tmux-powerline"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Don't forget to run <prefix>I to install the plugins!"
+echo "* Setting up link to $FULLDIR/tmux-powerline"
+ln -s "$FULLDIR"/tmux-powerline ~/.config
 
 echo "* Install n"
 curl -L https://git.io/n-install | bash
 
-echo "* Install required npm packages for vim, typescript, reveal, and yarn"
-sudo PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ~/n/bin/npm install -g neovim typescript ts-node reveal-md yarn
+echo "* Install required npm packages for vim, typescript"
+sudo PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ~/n/bin/npm install -g neovim typescript
 
 echo "* Install nvim plugins"
 cd ~ || exit
-vim +PlugInstall
-
-#vim -c 'CocInstall -sync coc-json coc-html coc-prettier coc-tsserver coc-eslint coc-pyright coc-rls coc-rust-analyzer|q'
-# vim +UpdateRemotePlugins
+nvim +PlugInstall
 
 echo "* Install Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+echo "* Install rls and rust-analyzer"
+~/.cargo/bin/rustup toolchain add nightly
+~/.cargo/bin/rustup component add rust-src rust-analysis rls
+
 echo "* Setting up link to $FULLDIR/config.toml"
 ln -s "$FULLDIR"/config.toml ~/.cargo
 
-#echo "* Install rls and rust-analyzer"
-#~/.cargo/bin/rustup toolchain add nightly
-#~/.cargo/bin/rustup component add rust-src rust-analysis rls
+#echo "* Install ruby and Jekyll for static pages"
+#sudo apt install -y ruby zlib
+#sudo gem install bundler
 
-#echo "* Install bandwhich"
-#~/.cargo/bin/cargo install bandwhich
+echo "* Install docker"
+sudo $INSTALL_COMMAND docker
+sudo usermod -aG docker "$USER"
+# sudo systemctl restart docker.service
+# testing
+# docker run hello-world
 
-echo "* Install ruby and Jekyll for static pages"
-sudo apt install -y ruby-dev build-essential zlib1g-dev
-sudo gem install bundler
-
-#echo "* Setup supervisor"
-#echo_supervisord_conf | sudo tee /etc/supervisor/supervisord.conf
-#echo '[include]
-#files=conf.d/*.conf' | sudo tee -a /etc/supervisor/supervisord.conf
-#echo '[Unit]
-#Description=Supervisor daemon
-#Documentation=http://supervisord.org
-#After=network.target
-#[Service]
-#ExecStart=/usr/local/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-#ExecStop=/usr/local/bin/supervisorctl $OPTIONS shutdown
-#ExecReload=/usr/local/bin/supervisorctl $OPTIONS reload
-#KillMode=process
-#Restart=on-failure
-#RestartSec=42s
-#[Install]
-#WantedBy=multi-user.target
-#Alias=supervisord.service' | sudo tee /etc/systemd/system/supervisord.service
-#sudo systemctl daemon-reload
-#sudo systemctl start supervisord
-
-if [ "$RELEASE" = "Debian" ]; then
-  echo "* Install nginx"
-  deb https://nginx.org/packages/debian/ bullseye nginx
-  deb-src https://nginx.org/packages/debian/ bullseye nginx
-  sudo apt update
-  sudo apt install -y nginx
-
-  echo "* Install docker"
-  sudo apt remove -y docker docker-engine docker.io containerd runc
-  sudo apt install -y ca-certificates gnupg lsb-release
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  sudo apt update
-  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  sudo usermod -aG docker "$USER"
-  sudo systemctl restart docker.service
-fi
-
-if [ "$RELEASE" = "Ubuntu" ]; then
-  echo "* Install docker"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt update
-  sudo apt install -y docker-ce docker-ce-cli containerd.io
-  # not needed since the group is added on instsall
-  # sudo groupadd docker
-  sudo usermod -aG docker "$USER"
-  sudo systemctl restart docker.service
-  # testing
-  # docker run hello-world
-fi
+function install_from_aur() {
+  package_name="$1"
+  package_dir="$FULLDIR"/"$package_name"
+  git clone https://aur.archlinux.org/"$package_name".git "$package_dir"
+  cd "$package_dir"
+  makepkg
+  sudo pacman --noconfirm -U "$package_name"*pkg.tar.zst
+  cd "$FULLDIR"
+  rm -rf "$package_dir"
+}
 
 if [ "$INSTALL_EXTRA" = true ]; then
-  echo "* Install dotnet core"
-  wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-  sudo dpkg -i packages-microsoft-prod.deb
-  sudo apt install -y dotnet-sdk-2.1
-
-  echo "* Install mongodb 4.2"
-  wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-  echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-  sudo apt update
-  sudo apt install -y mongodb-org
-
-  echo "* Install influxdb"
-  wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-  source /etc/lsb-release
-  echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-  sudo apt update
-  sudo apt install -y influxdb
-  sudo systemctl start influxdb
-
-  echo "* Install postgres"
-  sudo apt install -y postgresql
-
-  echo "* Install nginx"
-  sudo add-apt-repository ppa:nginx/stable
-  sudo apt install -y nginx
-
   echo "Installing gcloud CLI" # https://cloud.google.com/sdk/gcloud/
-  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-  sudo apt update
-  sudo apt install google-cloud-sdk
+  install_from_aur google-cloud-sdk
   gcloud init
   gcloud auth configure-docker
+  cd "$FULLDIR"
+
+  echo "Installing openvpn DNS updater"
+  install_from_aur openvpn-update-systemd-resolved
+
+  sudo $INSTALL_COMMAND clang git-cliff
 fi
 
 if [ "$INSTALL_GUI" = true ]; then
   echo "GUI applications"
-  echo "* Install meld (diffing), remmina (RDP to Windows), firefox"
-  sudo apt install -y meld remmina firefox
+  echo "* Install emoji fonts"
+  sudo $INSTALL_COMMAND noto-fonts-emoji
+
+  echo "* Install firefox"
+  sudo $INSTALL_COMMAND firefox
 
   echo "* Install tixati"
-  wget 'https://download2.tixati.com/download/tixati_2.83-1_amd64.deb'
-  sudo dpkg -i tixati_2.83-1_amd64.deb
+  sudo $INSTALL_COMMAND gtk2 traceroute dbus-glib
+  install_from_aur tixati
 
   echo "* Install discord"
-  wget 'https://discord.com/api/download?platform=linux&format=deb' -O discord.deb
-  sudo dpkg -i discord.deb
-
-  echo "* Install pandoc: check https://github.com/jgm/pandoc/releases for deb"
-  echo "* Install texlive, pandoc requirement for pdf"
-  sudo apt install texlive
+  sudo $INSTALL_COMMAND discord
 
   echo "* Install brave browser"
-  curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-  echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-  sudo apt install brave-browser
+  install_from_aur brave-bin
 
   echo "* Install telegram"
-  sudo apt install telegram-desktop
+  sudo $INSTALL_COMMAND telegram-desktop
 
   echo "* Setup udev for ledger"
-  wget -q -O - https://raw.githubusercontent.com/LedgerHQ/udev-rules/master/add_udev_rules.sh | sudo bash
+  curl -sSfL https://raw.githubusercontent.com/LedgerHQ/udev-rules/master/add_udev_rules.sh | sudo bash
 
   echo "* Install slack"
-  echo "You will need to do this yourself, please confirm when this is done"
-  read -r nothing
-  echo "$nothing"
+  install_from_aur slack-desktop
+
+  echo "* Install signal"
+  sudo $INSTALL_COMMAND signal-desktop
+
+  echo "Replace pulse audio with pipewire"
+  sudo $INSTALL_COMMAND pipewire pipewire-pulse
 fi
 
-if [ "$RASPBERRY_PI" = true ]; then
-  sudo pip3 install RPi.GPIO
-fi
-
-# WINDOWS ONLY
-# steam
-# windows terminal + keys and setups
-
+# Zoom download + install
 # GitHub ssh token
 # GITHUB_FILE=/home/jon/.ssh/github_id_rsa
-# echo "$GITHUB_FILE" | ssh-keygen -t rsa -b 4096 -C "joncinque@pm.me"
+# echo "$GITHUB_FILE" | ssh-keygen -t rsa -b 4096 -C "me@jonc.dev"
 # echo "* Add public key to GitHub:"
 # cat "$GITHUB_FILE".pub
