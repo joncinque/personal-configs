bd29aebf8a7adc1871a2ce1640fe24bee616632d
Jon C <me@jonc.dev>
2025-05-21 23:58:05 -0400

    nixos: Enable flakes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,9 @@
     ./users.nix
   ];
 
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
