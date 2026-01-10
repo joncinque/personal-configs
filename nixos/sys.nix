5bae6f0d271ebf83e54de95589c2ab46ac9a4beb
Jon C <me@jonc.dev>
2026-01-10 14:16:40 +0100

    nix: Update system every week

--- nixos/sys.nix
+++ nixos/sys.nix
@@ -9,6 +9,7 @@
     systemCronJobs = [
       #"*/5 * * * * jon cd /home/jon/src/epaper/e-Paper/RaspberryPi_JetsonNano/python && ./venv/bin/python3 examples/hi.py > /dev/null 2>&1"
       "7 11 * * * jon /home/jon/src/sys/daily.sh 2>&1"
+      "30 4 * * 0     root   /run/current-system/sw/bin/nix-channel --update && /run/current-system/sw/bin/nixos-rebuild switch && /run/current-system/sw/bin/shutdown -r now"
     ];
   };
 }
