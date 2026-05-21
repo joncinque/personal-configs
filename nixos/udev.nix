c60ab90eac4169e552516935d7b6ccea2d70c30f
Jon C <me@jonc.dev>
2026-05-21 10:45:03 +0200

    nixos: Add keystone udev rule

--- nixos/udev.nix
+++ nixos/udev.nix
@@ -6,5 +6,8 @@
       trezor-udev-rules
       # potentially even more if you need them
   ];
+  services.udev.extraRules = ''
+    # Keystone rules
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1209", ATTR{idProduct}=="3001", MODE="0660", TAG+="uaccess"
+  '';
 }
-
